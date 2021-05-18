61d853b0277de0dc67a3ebbec17bbfb10ffaffaa
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-18 15:26:12 -0600

    Working Design

--- game.js
+++ game.js
@@ -1,12 +1,27 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, onSnake, 
+getSnakeHead, snakeIntersection } from './snake.js';
+
+import {outsideGrid} from './grid.js'
+
+import {update as updateFood, draw as drawFood} from './food.js'
+
 let lastRenderTime = 0
-const SNAKE_SPEED = 2;
+const gameBoard = document.getElementById('game-board')
+let gameOver = false;
+
+
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm("You lost. Press ok to restart"))
+            window.location = '/'
+        return 
+    }
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED)
         return;
+    console.log('Render');
     window.requestAnimationFrame(main);
     lastRenderTime = currentTime;
-    console.log(currentTime);
 
     update();
     draw();
@@ -14,11 +29,18 @@ function main(currentTime) {
 
 window.requestAnimationFrame(main);
 
-
 function update() {
-
+    updateSnake();
+    updateFood();
+    checkDeath()
 }
 
 function draw() {
-    
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
